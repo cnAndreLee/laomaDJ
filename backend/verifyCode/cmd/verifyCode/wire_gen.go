27900199e4ad8da60a9e184767e73746eb9b4491
andre <cnandrelee@gmail.com>
2023-09-12 15:02:58 +0000

    generate ./...

--- backend/verifyCode/cmd/verifyCode/wire_gen.go
+++ backend/verifyCode/cmd/verifyCode/wire_gen.go
@@ -7,14 +7,17 @@
 package main
 
 import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
 	"verifyCode/internal/biz"
 	"verifyCode/internal/conf"
 	"verifyCode/internal/data"
 	"verifyCode/internal/server"
 	"verifyCode/internal/service"
+)
 
-	"github.com/go-kratos/kratos/v2"
-	"github.com/go-kratos/kratos/v2/log"
+import (
+	_ "go.uber.org/automaxprocs"
 )
 
 // Injectors from wire.go:
